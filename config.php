8f2decc764794725d886579edf126015cfbafb0e
Faisal Ahmed <faisal95bd@gmail.com>
2020-05-15 16:54:20 +0600

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = [
-  "secret_key"      => "sk_test_QOEPKH39wpj8mSErJYKcdnO600xv8VCwTq",
   "publishable_key" => "pk_test_oS6VU2vG4z3P1C3mlMuZwdia004n6GoBT4",
+  "secret_key"    => "sk_test_QOEPKH39wpj8mSErJYKcdnO600xv8VCwTq",
 ];
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
