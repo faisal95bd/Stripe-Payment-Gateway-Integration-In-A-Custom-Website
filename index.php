b13bbb7252007a19538c5041f0aaceeb9bfa8b81
Faisal Ahmed <faisal95bd@gmail.com>
2020-05-15 18:15:59 +0600

    index file updated

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php require_once('./config.php'); ?>
 
 <h1 style = "text-align: center;">My Website</h1>
-
 <hr><br>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quibusdam magnam, nihil tempore maxime quod, repellat tenetur non perspiciatis fugit, saepe rerum vero quam quo voluptatibus consequuntur voluptatem eum accusamus!</p>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure asperiores quo nisi odio dolor facere rerum a aspernatur illo aliquam, sapiente possimus ad ipsam expedita, alias ex hic dolore fugiat? </p><br>
